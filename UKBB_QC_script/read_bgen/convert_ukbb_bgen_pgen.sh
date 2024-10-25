83c56fe38ae0ec83118a383295c42653219e013f
Lang Liu <liulang@beluga1.int.ets1.calculquebec.ca>
2024-10-25 17:42:25 -0400

    delete stupid scripts

--- UKBB_QC_script/read_bgen/convert_ukbb_bgen_pgen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-out=$1
-chr=$2
-out=${out}/ukb22828_c${chr}_b0_v3
-plink2 \
-    --bgen /lustre03/project/6008063/neurohub/ukbb/new/Bulk/Imputation/UKB_imputation_from_genotype/ukb22828_c${chr}_b0_v3.bgen \
-    --sample /lustre03/project/6008063/neurohub/ukbb/new/Bulk/Imputation/UKB_imputation_from_genotype/ukb22828_c${chr}_b0_v3.sample \
-    --make-pgen \
-    --out ${out}
--- UKBB_QC_script/read_bgen/step2_convert_ukbb_bgen_pgen_by_chr18.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=0-3:00
-#SBATCH --account=def-grouleau
-#SBATCH --output=step2_convert_ukbb_bgen_pgen_by_chr18.out
-#SBATCH --mem=100g
-#SBATCH --cpus-per-task=40
-out=~/scratch/genotype/UKBB_new_imputation/
-bash /home/liulang/runs/lang/scripts/UKBB_QC_script/convert_ukbb_bgen_pgen.sh $out 18
-
